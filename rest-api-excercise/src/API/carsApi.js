ad463c80a6353fc01f35d841b30acd5338423103
Jon <19751518+JonChica@users.noreply.github.com>
2019-03-02 12:30:18 +0100

    api-rest with axios and async-await

--- rest-api-excercise/src/API/carsApi.js
+++ rest-api-excercise/src/API/carsApi.js
@@ -1,1 +1,40 @@
+import axios from 'axios';
+import 'babel-polyfill';
+
 const baseUrl = 'http://localhost:3050';
+
+// getAllCars () axios
+export const getAllCars = async () => {
+  try {
+    const response = await axios.get(`${baseUrl}/api/cars`);
+    return new Promise((resolve, _) => {
+      const cars = response.data;
+      resolve(cars);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getCarById = async id => {
+  try {
+    const response = await axios.get(`${baseUrl}/api/cars/${id}`);
+    return new Promise((resolve, _) => {
+      const car = response.data;
+      resolve(car);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const addCar = async car => {
+  try {
+    const response = await axios.post(`${baseUrl}/api/cars/`, car);
+    return new Promise((resolve, _) => {
+      resolve(response.statusText);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
