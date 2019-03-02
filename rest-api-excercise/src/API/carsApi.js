59b006aea0988c59d2ae40bf6837985f3aa17c9d
Jon <19751518+JonChica@users.noreply.github.com>
2019-03-02 13:49:50 +0100

    api-rest added fetch with promises

--- rest-api-excercise/src/API/carsApi.js
+++ rest-api-excercise/src/API/carsApi.js
@@ -3,7 +3,7 @@ import 'babel-polyfill';
 
 const baseUrl = 'http://localhost:3050';
 
-// getAllCars () axios
+// getAllCars () axios & async-await
 export const getAllCars = async () => {
   try {
     const response = await axios.get(`${baseUrl}/api/cars`);
@@ -16,6 +16,17 @@ export const getAllCars = async () => {
   }
 };
 
+// getAllCars () fetch & Promises
+export const getAllCarsFetch = () => {
+  const cars = fetch(`${baseUrl}/api/cars`)
+    .then(response => response.json())
+    .catch(e => console.log(e));
+  return new Promise((resolve, _) => {
+    resolve(cars);
+  });
+};
+
+// getCarById () axios & async-await
 export const getCarById = async id => {
   try {
     const response = await axios.get(`${baseUrl}/api/cars/${id}`);
@@ -28,6 +39,17 @@ export const getCarById = async id => {
   }
 };
 
+// getCarById () fetch & Promises
+export const getCarByIdFetch = id => {
+  const car = fetch(`${baseUrl}/api/cars/${id}`)
+    .then(response => response.json())
+    .catch(e => console.log(e));
+  return new Promise((resolve, _) => {
+    resolve(car);
+  });
+};
+
+// addCar () axios & async-await
 export const addCar = async car => {
   try {
     const response = await axios.post(`${baseUrl}/api/cars/`, car);
@@ -37,4 +59,19 @@ export const addCar = async car => {
   } catch (error) {
     console.log(error);
   }
+};
+
+// addCar () fetch & Promises
+export const addCarFetch = car => {
+  const response = fetch(`${baseUrl}/api/cars/`, {
+    method: 'POST',
+    body: JSON.stringify(car),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).catch(e => console.log(e));
+
+  return new Promise((resolve, _) => {
+    resolve('OK');
+  });
 };
